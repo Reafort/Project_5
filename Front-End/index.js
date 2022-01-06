934a8f41d0da559da3f5170c37ea747ffb3c1920
Rea Catherine <rearicafort21@gmail.com>
2022-01-05 22:05:23 -0800

    validating html

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,7 +1,9 @@
+
    
        let cartDiv = document.getElementById('items')
 
-
+function showcart(){
+    cartDiv.innerHTML = ''
    let cart = localStorage.getItem('cart');
    if (cart != null ){
        cart = JSON.parse(cart)
@@ -9,9 +11,7 @@
    }
 
 
-
-    //cartDiv.innerHTML = " " ;
-        cart.forEach(element => {
+        cart.forEach((element,index) => {
             cartDiv.insertAdjacentHTML('beforeend',` 
         
         <tr>
@@ -19,16 +19,103 @@
         <td>${element.name}</td>
         <td> <input class="input-cart"type="number"/> </td>
         <td>${element.price}</td>
-        <td><button class="delete-btn"><i class="fa fa-trash"></i></button></td>
+        <td><button class="delete-btn" onclick="deleteItem(${index})"><i class="fa fa-trash"></i></button></td>
        
 </tr>
         
         `)
     });
 
+    const empty = document.getElementById("empty");
+    const notEmpty = document.getElementById("notEmpty");
+  
+    if (cart.length === 0) {
+        empty.style.display = 'inline';
+        notEmpty.style.display = 'none';
+      } else {
+        empty.style.display = 'none';
+        notEmpty.style.display = 'inline';
+      }
+    }
+
+
+      //removing item using splice//
+
+ function deleteItem(deleteItem) {
+
+var retrieveObject = JSON.parse(localStorage.getItem('cart'));
+retrieveObject.splice(deleteItem,1)
+localStorage.setItem('cart',JSON.stringify(retrieveObject))
+showcart()
+}
+
+showcart()
 
 
 
+//form
+function submitForm() {
+    
+    let form = document.getElementById("contactForm");
+    //make validation before creating object
+    const fname = document.getElementById('fname').value ;
+const lname = document.getElementById('lname').value;
+const email= document.getElementById('email').value
+const city = document.getElementById('city').value
+const address = document.getElementById('address').value
+
+//Object line to access the values and send it back to API
+
+if(fname == "" || lname == "" || email == "" ||city == ""||address == ""){
+    alert("please insert the missing data")
+}else{
+    let contact = {
+        firstName : fname,
+        lastName :lname ,
+        email : email,
+        city : city,
+        address : address
+    }
+
+//send it to API
+    let products = []
+    let cart = JSON.parse(localStorage.cart)
+    cart.forEach(item =>{
+        products.push(item._id)
+    })
+    console.log(contact)
+    console.log(products)
+    let sendOBj = {
+        contact,
+        products
+    }
+// fetch sending the body and receiving data
+
+    fetch ('http://localhost:3000/api/cameras/order',{
+        method:"POST",
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body:JSON.stringify(sendOBj)
+    }).then(res=>res.json()).then(data=>{
+        console.log(data)
+        localStorage.setItem('orderId',data.orderId)
+        window.location.href = './confirmation.html'
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+    //form.submit();
+ }
+
+
+
+
+
+
+
+    
+      
 
 
 
